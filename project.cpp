f63e68c6f194aca84bc24c2d6ed8678a45bd660f
kaiser5 <kaiser5@stolaf.edu>
2019-05-10 15:12:44 -0500

    project.cpp image.yaml

--- project.cpp
+++ project.cpp
@@ -15,6 +15,7 @@ void text_box() {
     append(yaml, "  len: 900\n");
 }
 
+/*
 void screen() { // Makes a screen
     Point2 pview, pobox, pinput;
     Color white, black;
@@ -33,35 +34,27 @@ void screen() { // Makes a screen
     view.drawBorder(10, white, black);
     append(yaml, "  children: [Image_1]\n");
 }
-
-/* Delete???
-void picture() {
-    Point2 pview;
-    Color purple;
-    purple.red = purple.blue = 100; purple.green = 0;
-    pview.x = 450; pview.y = 220; // Point2's for view
-    Rectangle view(pview, 100, 200);
-    view.setColor(purple);
-    view.drawView();
-
-}
 */
+
+void screen(){
+    add_yaml("boxes.yaml", {{"id", 1}, {"x", 20}, {"y", 20}, {"w", 900}, {"h", 450}, {"back", "white"}, {"border", "black"}, {"borderw", 10}, {"child", 1}});
+}
 
 int main() {
     init();
 
-    if (length_of(yaml) < 50) {
-        mem[100] = 0;
-    }
+//    if (length_of(yaml) < 50) {
+//        mem[100] = 0;
+//    }
 
-    write_at(mem, 100, "Description"); // Writes info from user to mem
-    Range t_range = find_value(yaml, "content:"); // Gets info from user
-    write_at(mem, 100, yaml, t_range); // Writes info from user to mem
+//    write_at(mem, 100, "Description"); // Writes info from user to mem
+//    Range t_range = find_value(yaml, "content:"); // Gets info from user
+//    write_at(mem, 100, yaml, t_range); // Writes info from user to mem
 
     yaml[0] = 0;
     screen();
-    add_yaml("image.yaml", {{"id", 1}, {"im", "cat.png"}, {"w", 880}, {"h", 430}});
+//    add_yaml("image.yaml", {{"id", 1}, {"im", "cat.png"}, {"w", 880}, {"h", 430}});
 
-    mem[1] = 0;
+//    mem[1] = 0;
     quit();
 }
