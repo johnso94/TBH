2afe29c9c4408aee23440a136df9899e434c9591
Kevin Nelson <nelson67@stolaf.edu>
2019-05-11 14:46:58 -0500

    Nav Updates

--- RPG_MAKER/Client.h
+++ RPG_MAKER/Client.h
@@ -14,13 +14,13 @@ public:
     bool is_int(char a);
 
     void run();
-    void write_current_location_to_txt(string current_location);
     void clear_screen();
     void navigation(vector<string> v);
     void combat(vector<string> v);
     void select(vector<string> v);
     void trim_off_whitespaces(vector<string> &v);
-    void trim_off_whitespaces_str(string &str);
+    void trim_off_whitespaces(string &str);
+    void set_player_info(string str, string str2);
 
     template <typename T>
     int findInVector(const vector<T> & vecOfElements, const T & element);
@@ -29,18 +29,13 @@ public:
     int get_current_event_progress(string current_event);
 
     string get_location_name(string str);
-    string get_location_text(string str);
-    string get_event_type(string str);
-    string get_current_location();
-    string get_text_at(string str, char c);
+    string get_location_script(string str);
     string get_event_info(string str,int progress);
-    string get_player_info();
+    string get_player_info(string str);
 
     vector<string> get_World_Map();
     vector<string> split(string,char c);
     vector<pair<string,string> >get_nearby_locations();
-
-    pair<string,int> get_str_and_int(string str);
 
 };
 
